b805ce8cb846624811e9157c53f01a933792d377
Gokulsrinath2022 <143394375+Gokulsrinath2022@users.noreply.github.com>
2023-08-28 14:42:15 +0530

    my web pages

---
+++ login.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>login</title>
+    <link rel="stylesheet" type="text/css" href="login.css">
+</head>
+<body>
+     <form action="login.php" method="post" class="form">
+                                        <h2>Login Here</h2>
+                                        <input type="email" name="email" placeholder="Enter Email Here">
+                                        <input type="password" name="" placeholder="Enter Password Here ">
+                                        <button class="btnn"><a href="Login"><h3>log in</h3></a></button>
+                                    <p class="link">Don't have an account</p>
+                                    <a href="#">Sign up here</a>
+                                    <p class="liw">Log in with</p>
+                                    
+                                   </div> 
+</form>                    
+
+</body>
+</html>
